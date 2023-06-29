f41bf6356503bc3ba793ca686e82fe54a115681d
saher srur <srur173@gmail.com>
2023-06-29 18:14:42 +0300

    saher api

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.get('/ping', (req, res) => {
-res.send('PONG');
+app.get('/', (req, res) => {
+    res.send('saher api');
 });
 
 app.use((err, req, res, next) => {
-console.error(err);
-res.status(500).send('Server Error!');
+    console.error(err);
+    res.status(500).send('Server Error!');
 });
 
-app.listen(port,() => {
-console.log('listen to port', port);
+app.listen(port, () => {
+    console.log('listen to port', port);
 });
